62e19d03ca7174efa868004a55eb711ab41377c7
Victor Guerra Veloso <victorgvbh@gmail.com>
2025-02-20 02:24:57 -0500

    Create an utility assertion expectsException that takes Callback

--- 7035137/TestClass.java
+++ 7035137/TestClass.java
@@ -1,26 +1,39 @@
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class TestClass {
     private Object obj = new Object();
     
+    interface Callback {
+        void call() throws Exception;
+    }
+
+    void expectsException(Callback callback) {
+        try {
+            callback.call();
+            fail("ExpectedException not thrown");
+        } catch (Exception e) {
+            if (!(e instanceof ExpectedException)) {
+                fail("Expected ExpectedException, got " + e.getClass());
+            }
+            assertEquals("Expected message", e.getMessage());
+        }
+    }
+
     @Test
     public void testCase() {
-        try {
-            // assert 1
-            obj.do(1);
-            fail();
-        } catch (ExpectedException e) {
-            assertEquals("Expected message", e.getMessage());
-        }
+        expectsException(new Callback() {
+            public void call() throws Exception {
+                // assert 1
+                obj.do(1);
+            }
+        });
 
-        try {
-            // assert 2
-            obj.do(2);
-            fail();
-        } catch (ExpectedException e) {
-            assertEquals("Expected message", e.getMessage());
-        }
+        expectsException(new Callback() {
+            public void call() throws Exception {
+                // assert 2
+                obj.do(2);
+            }
+        });
     }
 } 
