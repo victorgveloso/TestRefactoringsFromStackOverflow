b5f3f75d98c192b35374da2088d1926d1a78a6c5
Victor Guerra Veloso <victorgvbh@gmail.com>
2025-02-20 02:12:17 -0500

    write your own test runner

--- 3437962/MyObjectTest.java
+++ 3437962/MyObjectTest.java
@@ -1,7 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class MyObjectTest {
     private MyObject myObj;
     
@@ -10,13 +13,24 @@ public class MyObjectTest {
         myObj = new MyObject(...);
     }
 
-    @Test
-    public void testSomeInputGivesExpectedOutput() {
-        assertEquals(expectedOutput, myObj.myFunction(someInput));
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][] {
+            {someInput, expectedOutput},
+            {someOtherInput, expectedOtherOutput}
+        };
     }
-
+    
+    private final Object input;
+    private final Object expected;
+    
+    public MyObjectTest(Object input, Object expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+    
     @Test
-    public void testSomeOtherInputGivesExpectedOutput() {
-        assertEquals(expectedOtherOutput, myObj.myFunction(someOtherInput));
+    public void testFunctionality() {
+        assertEquals(expected, myObj.myFunction(input));
     }
 } 
