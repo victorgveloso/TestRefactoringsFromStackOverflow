e0fbc71ae099fac0a7a4fa20337f82992c887eef
Victor Guerra Veloso <victorgvbh@gmail.com>
2025-02-20 01:49:04 -0500

    add a listener to jetty's life cycle

--- 15431708/JettyInitializationTest.java
+++ 15431708/JettyInitializationTest.java
@@ -1,17 +1,37 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.component.AbstractLifeCycleListener;
+import org.eclipse.jetty.util.component.LifeCycle;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import java.util.Set;
+import java.util.HashSet;
 
 public class JettyInitializationTest {
 
-    public static class I implements ServletContainerInitializer {
+    public static class MyInitializer implements ServletContainerInitializer {
         public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
             Servlet servlet = new MyServlet();
             ctx.addServlet("foo", servlet).addMapping("/*");
+        }
+    }
+
+    public static class JettyStartingListener extends AbstractLifeCycleListener {
+        private final ServletContext sc;
+        
+        public JettyStartingListener(ServletContext sc) {
+            this.sc = sc;
+        }
+        
+        @Override
+        public void lifeCycleStarting(LifeCycle event) {
+            try {
+                new MyInitializer().onStartup(new HashSet<Class<?>>(), sc);
+            } catch (ServletException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -23,9 +43,8 @@ public class JettyInitializationTest {
         Server server = new Server(0);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         
-        // Duplicated initialization
-        Servlet servlet = new MyServlet();
-        context.addServlet(servlet, "/*");
+        // Apply patch to remove duplication
+        context.addLifeCycleListener(new JettyStartingListener(context.getServletContext()));
         
         server.setHandler(context);
         server.start();
